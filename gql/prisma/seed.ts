b775dccadaddd5ffa701c7ade653a3f138cb93a7
marekgrzegorczyk <grigori93@gmail.com>
2023-10-08 22:06:03 +0200

    added prisma schema to gql server

---
+++ gql/prisma/seed.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
+ 
+const prisma = new PrismaClient();
+ 
+const productsCount = 5;
+ 
+for (let i = 0; i < productsCount; i++) {
+  const name = faker.commerce.productName();
+ 
+  const createdProduct = await prisma.product.create({
+    data: {
+      name: name,
+      slug: faker.helpers.slugify(name).toLowerCase(),
+      description: faker.commerce.productDescription(),
+      price: Number(faker.commerce.price()) * 100,
+    },
+  });
+  console.log(`Created product with id: ${createdProduct.id}`);
+}
--- gql/src/graphql/schema/resolvers/Query/product.ts
+++ gql/src/graphql/schema/resolvers/Query/product.ts
@@ -1,16 +1,15 @@
-import { faker } from "@faker-js/faker";
+import { prisma } from "../../../../../prisma/src/db";
 import type { QueryResolvers } from "./../../../types.generated";
+
 export const product: NonNullable<QueryResolvers["product"]> = async (
 	_parent,
 	arg,
 	_ctx,
 ) => {
-	const name = faker.commerce.productName();
-	return {
-		id: arg.id,
-		name,
-		slug: faker.helpers.slugify(name),
-		price: faker.number.int({ min: 100, max: 10000 }),
-		description: faker.commerce.productDescription(),
-	};
+	const product = await prisma.product.findUnique({
+		where: {
+			id: arg.id,
+		},
+	});
+	return product;
 };
