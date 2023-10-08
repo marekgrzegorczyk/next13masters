90b697aa71897d7e4a267abe9bb5ab570645fe12
marekgrzegorczyk <grigori93@gmail.com>
2023-10-08 21:46:16 +0200

    working gql server with faker generated record

--- gql/src/graphql/schema/resolvers/Query/product.ts
+++ gql/src/graphql/schema/resolvers/Query/product.ts
@@ -1,8 +1,16 @@
+import { faker } from "@faker-js/faker";
 import type { QueryResolvers } from "./../../../types.generated";
 export const product: NonNullable<QueryResolvers["product"]> = async (
 	_parent,
-	_arg,
+	arg,
 	_ctx,
 ) => {
-	/* Implement Query.product resolver logic here */
+	const name = faker.commerce.productName();
+	return {
+		id: arg.id,
+		name,
+		slug: faker.helpers.slugify(name),
+		price: faker.number.int({ min: 100, max: 10000 }),
+		description: faker.commerce.productDescription(),
+	};
 };
